63aba39f445764cc9bfb296741efbde13d9ac69c
Marco Roth <marco.roth@intergga.ch>
2019-03-10 01:17:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Xing Contact Photo Provider for ShortLook
+
+Show Xing users' profile pictures in ShortLook when you receive a Xing notification!
+
+(This is not ShortLook itself, but a plugin for it)
---
+++ XingContactPhotoProvider.h
@@ -0,0 +1,5 @@
+#import "ShortLook-API.h"
+
+@interface XingContactPhotoProvider : NSObject <DDNotificationContactPhotoProviding>
+  - (DDNotificationContactPhotoPromiseOffer *)contactPhotoPromiseOfferForNotification:(DDUserNotification *)notification;
+@end
